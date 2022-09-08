ce61cffffc35b57b8266e6b9021d6642ad983787
Ahaubro <73533051+Ahaubro@users.noreply.github.com>
2022-09-08 16:51:32 +0200

    Both updateScreens have full functionality

--- screens/Home/UpdateGoalScreen.tsx
+++ screens/Home/UpdateGoalScreen.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import {View, Pressable, Image, Text, StyleSheet, TextInput} from 'react-native'
+import { View, Pressable, Image, Text, StyleSheet, TextInput } from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { HomeNavigatorParamList } from '../../types/NavigationTypes'
 import { RouteProp } from '@react-navigation/native'
-import {useEditStatusMutation, useGetBooksByUserIdQuery, Book} from '../../redux/services/bookApi'
-import {useAddStatusUpdateMutation} from '../../redux/services/statusUpdateApi'
-import {useSetBooksGoalMutation, useResetBooksReadMutation} from '../../redux/services/userApi'
+import { useEditStatusMutation, useGetBooksByUserIdQuery, Book } from '../../redux/services/bookApi'
+import { useAddStatusUpdateMutation } from '../../redux/services/statusUpdateApi'
+import { useSetBooksGoalMutation, useResetBooksReadMutation } from '../../redux/services/userApi'
 
 
 type UpdateGoalScreenNavigationProps = StackNavigationProp<HomeNavigatorParamList, "UpdateGoalScreen">
@@ -18,12 +18,12 @@ interface UpdateGoalScreenProps {
 }
 
 
-const UpdateGoalScreen: React.FC<UpdateGoalScreenProps> = ({navigation, route}) => {
+const UpdateGoalScreen: React.FC<UpdateGoalScreenProps> = ({ navigation, route }) => {
 
-    const {bookId, userId} = route.params
+    const { bookId, userId } = route.params
 
     const userBooks = useGetBooksByUserIdQuery(userId)
-    const currentlyReadingBook : Book | undefined = userBooks.data?.books.find(book => book.bookId == bookId)
+    const currentlyReadingBook: Book | undefined = userBooks.data?.books.find(book => book.bookId == bookId)
 
     const [editBookStatus] = useEditStatusMutation()
 
@@ -44,22 +44,22 @@ const UpdateGoalScreen: React.FC<UpdateGoalScreenProps> = ({navigation, route}) 
     const [message, setMessage] = useState("")
 
     return <>
-        <View style={{margin: 20}}>
+        <View style={{ margin: 20 }}>
 
             <Pressable onPress={() => { navigation.navigate('Home') }}>
                 <Ionicons name={'chevron-back'} size={25} color={'black'} />
             </Pressable>
 
-            <View style={{flex: 1, alignItems: "center", marginTop: 5}}>
+            <View style={{ flex: 1, alignItems: "center", marginTop: 5 }}>
 
-                    {/* HERFRA SKAL PÅ EN NY SIDE */}
-
-
+                {/* HERFRA SKAL PÅ EN NY SIDE */}
 
 
-                <View style={{marginTop: 15}}>
+
+
+                <View style={{ marginTop: 15 }}>
                     <Text style={{ fontWeight: "700", fontSize: 12 }}>Reading goal:</Text>
-                    
+
                     <TextInput keyboardType="number-pad" placeholder="Enter book count" placeholderTextColor={"#AAA"} onChangeText={(books) => {
                         setBooksGoal(Number(books))
                     }} style={styles.textInput}></TextInput>
@@ -74,31 +74,31 @@ const UpdateGoalScreen: React.FC<UpdateGoalScreenProps> = ({navigation, route}) 
                     }
                 </Pressable>
 
-                <Text style={{color: "#F00"}}>{message}</Text>
+                <Text style={{ color: "#F00" }}>{message}</Text>
 
                 <Pressable style={{ ...styles.buttonGray, marginTop: 15 }} onPress={(() => {
-                    if(finishedBook)
-                        editBookStatus({userId, bookId, bookStatus: "History"})
+                    if (finishedBook)
+                        editBookStatus({ userId, bookId, bookStatus: "History" })
 
-                    if(resetProgress)
+                    if (resetProgress)
                         resetBooksRead(userId)
-                    
+
                     setMessage("")
 
-                    if(minutesRead != NaN){
-                        if(minutesRead > 0)
-                            addStatusUpdate({userId, minutesAdded: minutesRead})
-                    }else
+                    if (minutesRead != NaN) {
+                        if (minutesRead > 0)
+                            addStatusUpdate({ userId, minutesAdded: minutesRead })
+                    } else
                         setMessage("Minutes read must be a number!")
-                    
-                    
-                    if(booksGoal != NaN){
-                        if(booksGoal > 0)
-                            updateBooksGoal({userId, booksGoal: booksGoal})
-                    }else
-                        setMessage((message.length > 0 ? message + "\n" : "") +  + "Books goal must be a number!")
-                    
-                    if(minutesRead != NaN && booksGoal != NaN)
+
+
+                    if (booksGoal != NaN) {
+                        if (booksGoal > 0)
+                            updateBooksGoal({ userId, booksGoal: booksGoal })
+                    } else
+                        setMessage((message.length > 0 ? message + "\n" : "") + + "Books goal must be a number!")
+
+                    if (minutesRead != NaN && booksGoal != NaN)
                         navigation.navigate("Home")
                 })}>
                     <Text style={{ fontWeight: "500", fontSize: 12 }}>Save</Text>
@@ -128,7 +128,7 @@ const styles = StyleSheet.create({
         borderColor: "gray",
         borderRadius: 8,
         paddingHorizontal: 15,
-        paddingVertical: 10, 
+        paddingVertical: 10,
         fontSize: 12,
         marginLeft: 5,
         marginRight: 5,
--- screens/Home/UpdateStatusScreen.tsx
+++ screens/Home/UpdateStatusScreen.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import {View, Pressable, Image, Text, StyleSheet, TextInput} from 'react-native'
+import { View, Pressable, Image, Text, StyleSheet, TextInput } from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { HomeNavigatorParamList } from '../../types/NavigationTypes'
 import { RouteProp } from '@react-navigation/native'
-import {useEditStatusMutation, useGetBooksByUserIdQuery, Book} from '../../redux/services/bookApi'
-import {useAddStatusUpdateMutation} from '../../redux/services/statusUpdateApi'
-import {useSetBooksGoalMutation, useResetBooksReadMutation} from '../../redux/services/userApi'
+import { useEditStatusMutation, useGetBooksByUserIdQuery, Book } from '../../redux/services/bookApi'
+import { useAddStatusUpdateMutation } from '../../redux/services/statusUpdateApi'
+import { useSetBooksGoalMutation, useResetBooksReadMutation } from '../../redux/services/userApi'
 
 type UpdateStatusScreenNavigationProps = StackNavigationProp<HomeNavigatorParamList, "UpdateStatus">
 type UpdateStatisScreenRouteProps = RouteProp<HomeNavigatorParamList, "UpdateStatus">
@@ -16,12 +16,12 @@ interface HomeScreenProps {
     route: UpdateStatisScreenRouteProps
 }
 
-const UpdateStatusScreen: React.FC<HomeScreenProps> = ({navigation, route}) => {
+const UpdateStatusScreen: React.FC<HomeScreenProps> = ({ navigation, route }) => {
 
-    const {bookId, userId} = route.params
+    const { bookId, userId } = route.params
 
     const userBooks = useGetBooksByUserIdQuery(userId)
-    const currentlyReadingBook : Book | undefined = userBooks.data?.books.find(book => book.bookId == bookId)
+    const currentlyReadingBook: Book | undefined = userBooks.data?.books.find(book => book.bookId == bookId)
 
     const [editBookStatus] = useEditStatusMutation()
 
@@ -42,13 +42,13 @@ const UpdateStatusScreen: React.FC<HomeScreenProps> = ({navigation, route}) => {
     const [message, setMessage] = useState("")
 
     return <>
-        <View style={{margin: 20}}>
+        <View style={{ margin: 20 }}>
 
             <Pressable onPress={() => { navigation.navigate('Home') }}>
                 <Ionicons name={'chevron-back'} size={25} color={'black'} />
             </Pressable>
 
-            <View style={{flex: 1, alignItems: "center", marginTop: 5}}>
+            <View style={{ flex: 1, alignItems: "center", marginTop: 5 }}>
 
                 <Text style={{ fontWeight: "700", fontSize: 12 }}>Currently reading</Text>
 
@@ -74,14 +74,53 @@ const UpdateStatusScreen: React.FC<HomeScreenProps> = ({navigation, route}) => {
                         <Ionicons name={'close-sharp'} size={15} color={'red'} />
                     }
                 </Pressable>
-                
-                <View style={{marginTop: 15}}>
+
+                <View style={{ marginTop: 15 }}>
                     <Text style={{ fontWeight: "700", fontSize: 12 }}>Minutes read:</Text>
 
                     <TextInput keyboardType="number-pad" placeholder="Enter minutes" placeholderTextColor={"#AAA"} onChangeText={(minutes) => {
                         setMinutesRead(Number(minutes))
                     }} style={styles.textInput}></TextInput>
                 </View>
+
+                <Pressable style={{ ...styles.buttonGray, marginTop: 15, flex: 1, flexDirection: 'row' }} onPress={(() => {
+                    setResetProgress(!resetProgress)
+                })}><Text style={{ fontWeight: "500", fontSize: 12 }}>Reset Progress</Text>
+                    {resetProgress ?
+                        <Ionicons name={'checkmark-sharp'} size={15} color={'green'} /> :
+                        <Ionicons name={'close-sharp'} size={15} color={'red'} />
+                    }
+                </Pressable>
+
+                <Text style={{ color: "#F00" }}>{message}</Text>
+
+                <Pressable style={{ ...styles.buttonGray, marginTop: 15 }} onPress={(() => {
+                    if (finishedBook)
+                        editBookStatus({ userId, bookId, bookStatus: "History" })
+
+                    if (resetProgress)
+                        resetBooksRead(userId)
+
+                    setMessage("")
+
+                    if (minutesRead != NaN) {
+                        if (minutesRead > 0)
+                            addStatusUpdate({ userId, minutesAdded: minutesRead })
+                    } else
+                        setMessage("Minutes read must be a number!")
+
+
+                    if (booksGoal != NaN) {
+                        if (booksGoal > 0)
+                            updateBooksGoal({ userId, booksGoal: booksGoal })
+                    } else
+                        setMessage((message.length > 0 ? message + "\n" : "") + + "Books goal must be a number!")
+
+                    if (minutesRead != NaN && booksGoal != NaN)
+                        navigation.navigate("Home")
+                })}>
+                    <Text style={{ fontWeight: "500", fontSize: 12 }}>Save</Text>
+                </Pressable>
 
             </View>
         </View>
@@ -107,7 +146,7 @@ const styles = StyleSheet.create({
         borderColor: "gray",
         borderRadius: 8,
         paddingHorizontal: 15,
-        paddingVertical: 10, 
+        paddingVertical: 10,
         fontSize: 12,
         marginLeft: 5,
         marginRight: 5,
